339ded85ce24f4f0eb2ea3555c3635b63d831475
PeperMint9 <lobetion@gmail.com>
2022-10-19 16:30:46 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20170392/example/w0701implicitintent/MainActivity.kt
@@ -0,0 +1,35 @@
+package kr.ac.kumoh.s20170392.example.w0701implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20170392.example.w0701implicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHomepage.setOnClickListener {
+            val uri = Uri.parse("http://bwoh.github.io")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnMap.setOnClickListener {
+            val uri = Uri.parse("geo:0, 0?z=17&q=경북 구미시 산호대로25길 25-5")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnTelephone.setOnClickListener {
+            val uri = Uri.parse("tel:010-4787-2969")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+}
